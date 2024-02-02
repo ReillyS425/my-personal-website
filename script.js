16d5cb6c91a46db3a08f8a576839b57373f9465e
Reilly Shofner <reilly.shofner@gmail.com>
2024-02-01 16:10:56 -0800

    Added Script for viewing images or hiding images in gallery

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const h2 = document.createElement("h2");
 h2.textContent = "About me:";
 
 document.querySelector("body").appendChild(h2);
+
+function toggleSection() {
+    const section = document.getElementById("img");
+    const sectionButton = document.getElementById("Show Images");
+    if (section.hidden) {
+        sectionButton.innerHTML = "Show Less";
+        section.hidden = false;
+    } else {
+        sectionButton.innerHTML = "Show Images";
+        section.hidden = true;
+    }
+}
